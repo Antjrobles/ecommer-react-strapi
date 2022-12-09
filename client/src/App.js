ee2ed7e031f04a5d9135cb5b2371c10727cb1fa6
Antonio Robles <antjrobles@gmail.com>
2022-12-09 11:24:02 +0100

    feat: ItemDetails.jsx added
    ItemDetails.jsx added, and fully working.

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+// @ts-ignore
+import React from "react";
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Home from "./scenes/home/Home";
